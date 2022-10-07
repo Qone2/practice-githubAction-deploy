e798fb987724ededffdffd2a08f60389b8b425cf
Qone2 <whrbdnjs33@naver.com>
2022-10-07 14:29:47 +0900

    add index page

---
+++ src/main/java/com/codestates/practicegithubActiondeploy/HelloController.java
@@ -0,0 +1,19 @@
+package com.codestates.practicegithubActiondeploy;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String index() {
+        return "hello!";
+    }
+
+
+}
